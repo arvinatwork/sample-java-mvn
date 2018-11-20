533b2ff61b9c936e7f69fd35222d19257cffc511
Arvin Aboque <john.arvin.aboque@asia.ing.com>
2018-11-20 11:46:07 +0800

    Initial commit

---
+++ src/main/java/com/cdaas/demomvn/DemoMvnApplication.java
@@ -0,0 +1,12 @@
+package com.cdaas.demomvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvnApplication.class, args);
+	}
+}
